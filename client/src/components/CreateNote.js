73e1a88b22d77cf24344988bbbaba95885bcddb0
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-07-12 21:40:53 +0200

    Set the logic to edit notes

--- client/src/components/CreateNote.js
+++ client/src/components/CreateNote.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function CreateNote() {
+  const { id } = useParams();
+
   const [users, setUsers] = useState([]);
   const [inputValue, setInputValue] = useState({
     user: users[0],
@@ -12,9 +14,12 @@ export default function CreateNote() {
     content: ""
   });
   const [date, setDate] = useState(new Date());
+  const [noteId, setNoteId] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     getUsers();
+    getNoteById();
   }, []);
 
   const getUsers = async () => {
@@ -30,30 +35,53 @@ export default function CreateNote() {
   const onSubmitNote = async e => {
     e.preventDefault();
     const { title, content, user } = inputValue;
-
     const newNote = {
       title: title,
       content: content,
       date: date,
       author: user
     };
+    if (isEditing) {
+        await axios.put(`http://localhost:4000/api/notes/${noteId}`, newNote);
+    } else {
+      await axios.post("http://localhost:4000/api/notes/", newNote);
+    }
 
-    await axios.post("http://localhost:4000/api/notes", newNote);
+    window.location.href = "/";
   };
 
   const onChangeDate = dateSelected => {
     setDate(dateSelected);
   };
 
+  const getNoteById = async () => {
+    if (id) {
+      const note = await axios.get(`http://localhost:4000/api/notes/${id}`);
+      const { title, author, content, date } = note.data;
+
+      setInputValue({
+        user: author,
+        title: title,
+        content: content
+      });
+      setDate(new Date(date));
+      setIsEditing(true);
+      setNoteId(id);
+    }
+  };
+
   return (
     <section className="col-md-6 offset-md-3">
       <article className="card card-body">
-        <h4>Create a Note</h4>
+        <h4>
+          {isEditing ? "Edit note" : "Create a Note"}
+        </h4>
         <form onSubmit={onSubmitNote}>
           <div className="form-group">
             <select
               className="form-control"
               name="user"
+              value={inputValue.author}
               onChange={onInputChange}
             >
               {users.map(user =>
@@ -70,6 +98,7 @@ export default function CreateNote() {
               type="text"
               name="title"
               onChange={onInputChange}
+              value={inputValue.title}
               required
             />
           </div>
@@ -81,6 +110,7 @@ export default function CreateNote() {
               cols="30"
               rows="10"
               onChange={onInputChange}
+              value={inputValue.content}
               required
             />
           </div>
@@ -91,11 +121,9 @@ export default function CreateNote() {
               onChange={onChangeDate}
             />
           </div>
-          <Link to="/">
-            <button type="submit" className="btn btn-primary">
-              Save Note
-            </button>
-          </Link>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
         </form>
       </article>
     </section>
--- client/src/components/NotesList.js
+++ client/src/components/NotesList.js
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { format } from "timeago.js";
 
 export default function NotesList() {
-  return <div>NotesList</div>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = async () => {
+    const res = await axios.get("http://localhost:4000/api/notes");
+    setNotes(res.data);
+    console.log(notes);
+  };
+
+  const deleteNote = async id => {
+    await axios.delete(`http://localhost:4000/api/notes/${id}`);
+    getNotes();
+  };
+
+  return (
+    <section className="row">
+      {notes.map(note =>
+        <section className="col-md-4 p-2" key={note._id}>
+          <article className="card">
+            <header className="card-header d-flex justify-content-between">
+              <h5>
+                {note.title}
+              </h5>
+              <Link to={`/edit/${note._id}`} className="btn btn-secondary">
+                Edit
+              </Link>
+            </header>
+            <section className="card-body">
+              <p>
+                {note.content}
+              </p>
+              <p>
+                {note.author}
+              </p>
+              <p>
+                {format(note.date)}
+              </p>
+            </section>
+            <footer className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteNote(note._id)}
+              >
+                Delete
+              </button>
+            </footer>
+          </article>
+        </section>
+      )}
+    </section>
+  );
 }
